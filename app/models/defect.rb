447ea23c2422cdc82fcd983f24d7b28e07d6fd02
Michal Musialik <michal@michalmusialik.com>
2014-11-19 13:54:48 +0100

    Make sure the created defects are valid

--- app/models/defect.rb
+++ app/models/defect.rb
@@ -1,3 +1,30 @@
 class Defect < ActiveRecord::Base
   belongs_to :machine
+  validate :defects_must_start_before_they_finish
+  validate :defects_cant_overlap
+
+  def mtbf
+    mean_time_between_failures
+  end
+
+  def mean_time_between_failures
+    
+  end
+
+  private
+
+  def defects_must_start_before_they_finish
+    if started_at > finished_at
+      errors.add(:started_at, "Start time cannot be before finish time")
+    end
+  end
+
+  def defects_cant_overlap
+    if self.class.where('machine_id = :machine_id AND started_at >= :started_at AND started_at <= :finished_at', machine_id: machine_id, started_at: started_at, finished_at: finished_at).any? or
+        self.class.where('machine_id = :machine_id AND finished_at >= :started_at AND finished_at <= :finished_at', machine_id: machine_id, started_at: started_at, finished_at: finished_at).any? or
+        self.class.where('machine_id = :machine_id AND started_at <= :started_at AND finished_at >= :finished_at', machine_id: machine_id, started_at: started_at, finished_at: finished_at).any?
+
+      errors.add(:base, "Defects can't overlap")
+    end
+  end
 end
